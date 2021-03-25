b4d97a582f9c4c1e1bf6101716113b530b5a8fa3
ChistEgor <61418907+ChistEgor@users.noreply.github.com>
2021-03-25 13:58:02 +0200

    Add files via upload

--- joins.sql
+++ joins.sql
@@ -21,32 +21,22 @@ SELECT pp.FirstName
         FROM HumanResources.Employee AS he
         WHERE pp.BusinessEntityID = he.BusinessEntityID
         ) AS JobTitle
-FROM Person.Person AS pp;
+FROM Person.Person AS pp
 GO
 
--- the same action but through a join. better
-SELECT pp.FirstName
-    , pp.LastName
-    , he.JobTitle
-FROM Person.Person AS pp
-FULL OUTER JOIN HumanResources.Employee AS he
-    ON (pp.BusinessEntityID = he.BusinessEntityID);
-GO
-
--- Task 1.3 *don't sure
-SELECT pp.FirstName
-    , pp.LastName
-    , (
-        SELECT he.JobTitle
-        FROM HumanResources.Employee AS he
-        WHERE pp.BusinessEntityID = he.BusinessEntityID
-        ) AS JobTitle
-FROM Person.Person AS pp
-WHERE (
-        SELECT he.JobTitle
-        FROM HumanResources.Employee AS he
-        WHERE pp.BusinessEntityID = he.BusinessEntityID
-        ) IS NOT NULL;
+-- Task 1.3
+SELECT TEMPORARY.*
+FROM (
+    SELECT pp.FirstName
+        , pp.LastName
+        , (
+            SELECT JobTitle
+            FROM HumanResources.Employee AS he
+            WHERE pp.BusinessEntityID = he.BusinessEntityID
+            ) AS JobTitle
+    FROM Person.Person AS pp
+    ) AS TEMPORARY
+WHERE JobTitle IS NOT NULL
 GO
 
 --Task 1.4
@@ -54,7 +44,7 @@ SELECT pp.FirstName
     , pp.LastName
     , he.JobTitle
 FROM Person.Person AS pp
-CROSS JOIN HumanResources.Employee AS he;
+CROSS JOIN HumanResources.Employee AS he
 GO
 
 --Task 1.5
@@ -65,14 +55,4 @@ FROM (
         , he.JobTitle
     FROM Person.Person AS pp
     CROSS JOIN HumanResources.Employee AS he
-    ) AS TemporaryTable;
-GO
-
---check
-SELECT (
-        SELECT COUNT(FirstName)
-        FROM Person.Person
-        ) * (
-        SELECT COUNT(JobTitle)
-        FROM HumanResources.Employee
-        ) AS Amount;
+    ) AS TemporaryTable
