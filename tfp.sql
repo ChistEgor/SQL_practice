9a23b5c46399dfb031b1c4d9a767b414639a909c
ChistEgor <61418907+ChistEgor@users.noreply.github.com>
2021-03-30 10:35:05 +0300

    completed tasks with tests

---
+++ tfp.sql
@@ -0,0 +1,88 @@
+USE AdventureWorks2019
+GO
+
+
+-- Task 4.1
+CREATE TRIGGER notifier
+ON HumanResources.Department
+AFTER INSERT, UPDATE
+AS
+    THROW 50001, 'To insert or update the HumanResources.Department you have to drop the trigger "notifier"', 1; -- lof
+    ROLLBACK;
+GO
+
+
+-- Task 4.2
+CREATE TRIGGER notifier2
+ON DATABASE
+AFTER ALTER_TABLE
+AS
+    THROW 50002, 'To alter table you have to drop the trigger "notifier2"', 1; -- lof
+    ROLLBACK;
+GO
+
+-- Test
+ALTER TABLE HumanResources.Department DROP COLUMN Address;
+GO
+SELECT * FROM HumanResources.Department
+GO
+
+
+-- Task 4.3
+CREATE FUNCTION dbo.ufnConcatStrings (
+    @column1 varchar(255)
+    , @column2 varchar(255)
+    )
+RETURNS varchar(255)
+AS
+BEGIN
+    DECLARE @concat_string varchar(255)
+
+    SET @concat_string = CONCAT_WS(' - ', @column1, @column2);
+
+    RETURN @concat_string;
+END;
+GO
+
+-- Test
+SELECT dbo.ufnConcatStrings('settle', 'down') AS ConcatString
+GO
+SELECT dbo.ufnConcatStrings(Name, GroupName) 
+FROM HumanResources.Department
+GO
+
+
+-- Task 4.4
+CREATE FUNCTION HumanResources.ufnEmployeeByDepartment (@departmentID INT)
+RETURNS TABLE
+AS
+RETURN 
+(
+    SELECT EH.DepartmentID, HE.*
+    FROM HumanResources.Employee AS HE
+    JOIN HumanResources.EmployeeDepartmentHistory AS EH
+        ON HE.BusinessEntityID = EH.BusinessEntityID
+    WHERE EH.DepartmentID = @departmentID
+);
+GO
+
+-- Test
+SELECT * FROM HumanResources.ufnEmployeeByDepartment(16)
+GO
+
+
+-- Task 4.5
+CREATE PROC Person.uspSearchByName 
+    @Name nvarchar(50) = '%'
+AS
+BEGIN
+    SELECT BusinessEntityId
+        , FirstName
+        , LastName
+    FROM Person.Person
+    WHERE FirstName LIKE @Name OR LastName LIKE @Name;
+END 
+GO
+
+-- Test
+EXEC Person.uspSearchByName N'Ada%';
