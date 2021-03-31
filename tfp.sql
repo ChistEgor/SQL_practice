973abeb810f9d5d9c83ce8ebffca2bc9f6de8e4f
ChistEgor <61418907+ChistEgor@users.noreply.github.com>
2021-03-31 23:32:22 +0300

    no doubt about that

--- tfp.sql
+++ tfp.sql
@@ -5,10 +5,9 @@ GO
 -- Task 4.1
 CREATE TRIGGER notifier
 ON HumanResources.Department
-AFTER INSERT, UPDATE
+INSTEAD OF UPDATE, INSERT
 AS
-    THROW 50001, 'To insert or update the HumanResources.Department you have to drop the trigger "notifier"', 1; -- lof
-    ROLLBACK;
+    THROW 50001, 'To insert or update the HumanResources.Department you have to drop the trigger "notifier"', 16;
 GO
 
 
@@ -17,20 +16,22 @@ CREATE TRIGGER notifier2
 ON DATABASE
 AFTER ALTER_TABLE
 AS
-    THROW 50002, 'To alter table you have to drop the trigger "notifier2"', 1; -- lof
-    ROLLBACK;
+    THROW 50002, 'To alter table you have to drop the trigger "notifier2"', 16;
 GO
 
 
 -- Task 4.3
+IF OBJECT_ID (N'dbo.ufnConcatStrings', N'FN') IS NOT NULL  
+    DROP FUNCTION dbo.ufnConcatStrings;
+    
 CREATE FUNCTION dbo.ufnConcatStrings (
-    @column1 varchar(255)
-    , @column2 varchar(255)
+    @column1 varchar(25)
+    , @column2 varchar(25)
     )
-RETURNS varchar(255)
+RETURNS varchar(53)
 AS
 BEGIN
-    DECLARE @concat_string varchar(255);
+    DECLARE @concat_string varchar(53);
     SET @concat_string = CONCAT_WS(' - ', @column1, @column2);
     RETURN @concat_string;
 END;
@@ -38,12 +39,15 @@ GO
 
 
 -- Task 4.4
+IF OBJECT_ID (N'HumanResources.ufnEmployeeByDepartment', N'IF') IS NOT NULL  
+    DROP FUNCTION HumanResources.ufnEmployeeByDepartment;
+    
 CREATE FUNCTION HumanResources.ufnEmployeeByDepartment (@departmentID INT)
 RETURNS TABLE
 AS
 RETURN 
 (
-    SELECT EH.DepartmentID, HE.*
+    SELECT HE.*
     FROM HumanResources.Employee AS HE
     JOIN HumanResources.EmployeeDepartmentHistory AS EH
         ON HE.BusinessEntityID = EH.BusinessEntityID
@@ -57,10 +61,13 @@ CREATE PROC Person.uspSearchByName
     @Name nvarchar(50) = '%'
 AS
 BEGIN
-    SELECT BusinessEntityId
-        , FirstName
-        , LastName
-    FROM Person.Person
-    WHERE FirstName LIKE @Name OR LastName LIKE @Name;
+    DECLARE @ReturnName NVARCHAR(25)
+	SET @ReturnName = '%' + @Name + '%'
+	SELECT BusinessEntityId
+		,FirstName
+		,LastName
+	FROM Person.Person
+	WHERE FirstName LIKE @ReturnName
+		OR LastName LIKE @ReturnName;
 END 
 GO
